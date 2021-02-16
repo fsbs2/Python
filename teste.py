fee2cf8cc796ab64435158ac08e0060cc8d413ba
Gabriel Fernandes <gabriel_fsbs2@hotmail.com>
2021-02-16 00:14:32 -0300

    exercicio tabuada

--- teste.py
+++ teste.py
@@ -1,5 +1,11 @@
-def maximo(x,y):
-    if(x>y):
-        return x
-    else:
-        return y
+def tabuada():
+    tab = 1
+    while tab <= 10:
+        print("Tabuada do", tab, ":", end="\t")
+        i = 1
+        while i <= 10:
+            print(tab*i, end = "\t")
+            i = i + 1
+        print()
+        tab = tab + 1
+tabuada()
